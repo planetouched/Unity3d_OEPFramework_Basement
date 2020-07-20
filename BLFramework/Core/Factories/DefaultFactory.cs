f1ddafde26fe96a7f9146a17ad70baa35798ae41
Stanislav Kramorov <stas.k@playgendary.com>
2020-07-20 11:15:19 +0400

    add virtual modifier to Factories

--- BLFramework/Core/Factories/DefaultFactory.cs
+++ BLFramework/Core/Factories/DefaultFactory.cs
@@ -4,32 +4,32 @@ namespace Basement.BLFramework.Core.Factories
 {
     public class DefaultFactory : IDefaultFactory
     {
-        public object Build(Type type)
+        public virtual object Build(Type type)
         {
             return Activator.CreateInstance(type);
         }
 
-        public object Build<T1>(Type type, T1 param1)
+        public virtual object Build<T1>(Type type, T1 param1)
         {
             return Activator.CreateInstance(type, param1);
         }
 
-        public object Build<T1, T2>(Type type, T1 param1, T2 param2)
+        public virtual object Build<T1, T2>(Type type, T1 param1, T2 param2)
         {
             return Activator.CreateInstance(type, param1, param2);
         }
 
-        public object Build<T1, T2, T3>(Type type, T1 param1, T2 param2, T3 param3)
+        public virtual object Build<T1, T2, T3>(Type type, T1 param1, T2 param2, T3 param3)
         {
             return Activator.CreateInstance(type, param1, param2, param3);
         }
 
-        public object Build<T1, T2, T3, T4>(Type type, T1 param1, T2 param2, T3 param3, T4 param4)
+        public virtual object Build<T1, T2, T3, T4>(Type type, T1 param1, T2 param2, T3 param3, T4 param4)
         {
             return Activator.CreateInstance(type, param1, param2, param3, param4);
         }
 
-        public object Build<T1, T2, T3, T4, T5>(Type type, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5)
+        public virtual object Build<T1, T2, T3, T4, T5>(Type type, T1 param1, T2 param2, T3 param3, T4 param4, T5 param5)
         {
             return Activator.CreateInstance(type, param1, param2, param3, param4, param5);
         }
