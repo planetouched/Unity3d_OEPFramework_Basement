6ec1d016e4372cbfcfa343035be19ccc0bf843c4
Ilya Bokovenko <chill.waters@gmail.com>
2022-03-17 17:40:34 +0300

    simple requirement

---
+++ BLFramework/Essential/Requirements/SimpleRequirement.cs
@@ -0,0 +1,20 @@
+﻿using Basement.BLFramework.Core.Context;
+using Basement.Common;
+
+namespace Basement.BLFramework.Essential.Requirements
+{
+    public class SimpleRequirement : Requirement
+    {
+        private bool _checked;
+        
+        public SimpleRequirement(RawNode node, IContext context) : base(node, context)
+        {
+            _checked = node.GetBool("checked");
+        }
+
+        public override bool Check()
+        {
+            return _checked;
+        }
+    }
+}
